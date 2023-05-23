f841599635e6f64b1f2353c33666f45a0ccf1320
vladislav1137 <mr.vladislav220@yandex.ru>
2023-05-23 23:19:48 +0300

    commit 2

--- src/main/java/com/example/mapemployee/model/Employee.java
+++ src/main/java/com/example/mapemployee/model/Employee.java
@@ -6,11 +6,32 @@ import java.util.Objects;
 public class Employee {
     private String firstName;
     private String lastName;
+    private Integer department;
+    private double salary;
 
-    public Employee(String firstName, String lastName) {
+    public Employee(String firstName, String lastName, Integer department, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
     }
+
+    public Integer getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Integer department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
 
     @Override
     public boolean equals(Object o) {
---
+++ src/main/java/com/example/mapemployee/service/DepartmentService.java
@@ -0,0 +1,41 @@
+package com.example.mapemployee.service;
+
+import com.example.mapemployee.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+@Service
+public class DepartmentService {
+    private final EmployeeService employeeService;
+
+    public DepartmentService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    public Employee getEmployeeWithMaxSalary(int department) {
+        return employeeService.getAll().stream()
+                .filter(employee -> employee.getDepartment() == department)
+                .max(Comparator.comparingDouble(Employee::getSalary))
+                .orElse(null);
+    }
+    public Employee getEmployeeWithMinSalary(int department) {
+        return employeeService.getAll().stream()
+                .filter(employee -> employee.getDepartment() == department)
+                .min(Comparator.comparingDouble(Employee::getSalary))
+                .orElse(null);
+    }
+
+    public List<Employee> getEmployeeByDepartment(int department) {
+        return employeeService.getAll().stream()
+                .filter(employee -> employee.getDepartment()== department)
+                .collect(Collectors.toList());
+    }
+
+    public Map<Integer, List<Employee>> getEmployeeGroup() {
+        return employeeService.getAll().stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+    }
+}
